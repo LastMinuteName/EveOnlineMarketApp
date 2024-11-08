16eac08b1c9de492ad9ad2bd0bae2c63e292adbd
LastMinuteName <LastMinuteName@hotmail.com>
2024-11-08 18:10:19 +1100

    added additional JSON pattern matching for edge cases where evetycoon returns 'infinity' in maxBuy and minSell fields

--- lib/model/entity/market_stats.dart
+++ lib/model/entity/market_stats.dart
@@ -57,6 +57,38 @@ class MarketStats {
           maxBuy: maxBuy,
           minSell: minSell,
         ),
+      /// Pattern to check if eveTycoon has returned a market statistic with a string of "infinity"
+      /// for either maxBuy or maxSell. This happens because there is no buy or sell orders on the market
+      /// at the current time and will therefore return "infinity" to reflect the lack of current market
+      /// demand/value.
+      {
+        'buyVolume': int buyVolume,
+        'sellVolume': int sellVolume,
+        'buyOrders': int buyOrders,
+        'sellOrders': int sellOrders,
+        'buyOutliers': int buyOutliers,
+        'sellOutliers': int sellOutliers,
+        'buyThreshold': double buyThreshold,
+        'sellThreshold': double sellThreshold,
+        'buyAvgFivePercent': double buyAvgFivePercent,
+        'sellAvgFivePercent': double sellAvgFivePercent,
+        'maxBuy': dynamic maxBuy,
+        'minSell': dynamic minSell,
+      } =>
+        MarketStats(
+          buyVolume: buyVolume,
+          sellVolume: sellVolume,
+          buyOrders: buyOrders,
+          sellOrders: sellOrders,
+          buyOutliers: buyOutliers,
+          sellOutliers: sellOutliers,
+          buyThreshold: buyThreshold,
+          sellThreshold: sellThreshold,
+          buyAvgFivePercent: buyAvgFivePercent,
+          sellAvgFivePercent: sellAvgFivePercent,
+          maxBuy: maxBuy is double ? maxBuy : double.infinity,
+          minSell: minSell is double ? minSell : double.infinity,
+        ),
       _ => throw const FormatException('Failed to load market stats.'),
     };
   }
