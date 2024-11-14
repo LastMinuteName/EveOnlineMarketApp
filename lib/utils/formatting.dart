067bdf4e6efb96b4360966a7e5901ce8ddcc8692
LastMinuteName <LastMinuteName@hotmail.com>
2024-11-14 17:55:31 +1100

    changed toCommaSeparated function to also format ints appropriately

--- lib/utils/formatting.dart
+++ lib/utils/formatting.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 
-String toCommaSeparated(double value) {
-  final numberFormatter = NumberFormat("#,##0.00", "en_US");
+String toCommaSeparated(num value) {
+  final numberFormatter = value is int ? NumberFormat("#,##0", "en_US") : NumberFormat("#,##0.00", "en_US");
 
   return numberFormatter.format(value);
 }
