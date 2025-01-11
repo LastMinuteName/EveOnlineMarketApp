c0d32eaf1519f561ea16f53f286fa1cf4e1bc219
LastMinuteName <LastMinuteName@hotmailcom>
2025-01-12 09:42:09 +1100

    added assetretriever class to handle image retrieving

--- android/app/src/main/java/com/evemarketapp/eve_online_market_application/AssetRetriever.kt
+++ android/app/src/main/java/com/evemarketapp/eve_online_market_application/AssetRetriever.kt
@@ -1,2 +1,30 @@
 package com.evemarketapp.eve_online_market_application
 
+import android.content.Context
+import android.content.res.AssetFileDescriptor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import io.flutter.FlutterInjector
+import io.flutter.embedding.engine.loader.FlutterLoader
+import java.io.InputStream
+
+fun getInvTypeIconBitmap(context : Context, typeID : Int) : Bitmap {
+    val loader : FlutterLoader = FlutterInjector.instance().flutterLoader()
+    lateinit var bmp : Bitmap
+    try {
+        val key : String = loader.getLookupKeyForAsset("assets/icons/invTypeIcons/${typeID}.png")
+        val fd : AssetFileDescriptor = context.assets.openFd(key)
+        val inputStream : InputStream = fd.createInputStream()
+        bmp = BitmapFactory.decodeStream(inputStream)
+
+        inputStream.close()
+    }
+    catch (_: Exception) {
+        val key : String = loader.getLookupKeyForAsset("assets/icons/invTypeIcons/0.png")
+        val fd : AssetFileDescriptor = context.assets.openFd(key)
+        val inputStream : InputStream = fd.createInputStream()
+        bmp = BitmapFactory.decodeStream(inputStream)
+    }
+
+    return bmp
+}
--- android/app/src/main/java/com/evemarketapp/eve_online_market_application/WatchlistGlanceWidget.kt
+++ android/app/src/main/java/com/evemarketapp/eve_online_market_application/WatchlistGlanceWidget.kt
@@ -3,14 +3,9 @@ package com.evemarketapp.eve_online_market_application
 import HomeWidgetGlanceState
 import HomeWidgetGlanceStateDefinition
 import android.content.Context
-import android.content.res.AssetFileDescriptor
-import android.content.res.AssetManager
-import android.graphics.BitmapFactory
-import android.graphics.drawable.Drawable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
@@ -22,13 +17,12 @@ import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.currentState
 import androidx.glance.layout.Box
+import androidx.glance.layout.Row
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
+import androidx.glance.layout.size
+import androidx.glance.layout.width
 import androidx.glance.text.Text
-import io.flutter.FlutterInjector
-import io.flutter.plugin.common.PluginRegistry
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.loader.FlutterLoader
-import java.io.InputStream
 
 
 class WatchlistGlanceWidget : GlanceAppWidget() {
@@ -46,12 +40,8 @@ class WatchlistGlanceWidget : GlanceAppWidget() {
     private fun GlanceContent(context: Context, currentState: HomeWidgetGlanceState) {
         val data = currentState.preferences
         val count = data.getInt("counter", 0)
-        val loader : FlutterLoader = FlutterInjector.instance().flutterLoader()
-        val key : String = loader.getLookupKeyForAsset("assets/icons/invTypeIcons/0.png")
-        val fd : AssetFileDescriptor = context.assets.openFd(key)
-        val inputStream : InputStream = fd.createInputStream()
-        val bmp = BitmapFactory.decodeStream(inputStream)
 
+        val typeIDs = listOf(44992, 62628, 4405, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
         MaterialTheme {
             Box(
@@ -60,16 +50,19 @@ class WatchlistGlanceWidget : GlanceAppWidget() {
                     .padding(16.dp)
             ) {
                 LazyColumn {
-                    item {
-                        Image(
-                            provider = ImageProvider(bmp),
-                            contentDescription = "an image",
-                        )
-                    }
-                    items(20) { index: Int ->
-                        Text(
-                            text = "Item $index",
-                        )
+                    items(typeIDs.size) { index: Int ->
+                        val bmp = getInvTypeIconBitmap(context, typeIDs[index])
+                        Row {
+                            Image(
+                                provider = ImageProvider(bmp),
+                                contentDescription = "an image",
+                                modifier = GlanceModifier
+                                    .size(64.dp)
+                            )
+                            Text(
+                                text = "ID ${typeIDs[index]}",
+                            )
+                        }
                     }
                 }
             }
